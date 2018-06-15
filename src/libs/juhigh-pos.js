8f769485bcb09e63d53f978368f53bae05d7e4e3
CooLNuanfeng <1634898698@qq.com>
2018-06-15 18:02:51 +0800

    text position

--- src/libs/juhigh-pos.js
+++ src/libs/juhigh-pos.js
@@ -16,7 +16,7 @@ module.exports = {
         },
         image: {
             gapX: 63,
-            gapY: 24,
+            gapY: 24.2,
             w: 90,
             h: 177
         },
@@ -54,27 +54,27 @@ module.exports = {
                 y: 0
             },
             text: {
-                x: 7,
-                y: -18
+                x: 9,
+                y: -23
             }
         },
         image: {
             gapX: 25,
-            gapY: 10,
+            gapY: 10.5,
             w: 45,
             h: 82
         },
         text: {
-            gapX: 18,
-            gapY: .9,
+            gapX: 19,
+            gapY: .68,
             scaleX: 1.3,
             scaleY: 1,
-            skewH: .6,
+            skewH: .5,
             skewV: -1.5,
             moveX: 2,
-            moveY: 32,
-            fontSizeZh: 11,
-            fontSizeEn: 18
+            moveY: 30,
+            fontSizeZh: 10,
+            fontSizeEn: 14
         },
         linebreak: {
             image: {
@@ -82,8 +82,8 @@ module.exports = {
                 y: 30
             },
             text: {
-                x: -.8,
-                y: 35.5
+                x: 12,
+                y: 26
             }
         }
     }
--- src/libs/juhigh-text.js
+++ src/libs/juhigh-text.js
@@ -36,6 +36,7 @@ Juhigh.prototype.drawCanvas = function(){
     t.words = t.words.replace(/\n/g, "£");
 
     t.canvas.transform(t.dimensions.text.scaleX, t.dimensions.text.skewH, t.dimensions.text.skewV, t.dimensions.text.scaleY, t.dimensions.text.moveX, t.dimensions.text.moveY);
+    t.canvas.rotate(-.5 * Math.PI / 180);
 
     t.words.split("").forEach((word)=>{
         if(word === "£"){
@@ -43,19 +44,20 @@ Juhigh.prototype.drawCanvas = function(){
             t.ty = t.base.ty + t.dimensions.linebreak.text.y * lines;
 
             lines = lines + 1;
+            return;
         }else{
             t.tx = t.tx + t.dimensions.text.gapX;
             t.ty = t.ty + t.dimensions.text.gapY;
-        }
-        color = "♥" === color ? "#ca2626" : "❤" === color ? "#d92b6d" : "#40210f";
-        t.canvas.fillStyle = color;
-        t.canvas.textAlign = "center";
-        t.canvas.beginPath();
+            color = "♥" === color ? "#ca2626" : "❤" === color ? "#d92b6d" : "#40210f";
+            t.canvas.fillStyle = color;
+            t.canvas.textAlign = "center";
+            t.canvas.beginPath();
 
-        encodeURIComponent(word).length > 1 ? t.canvas.font = "900 " + t.dimensions.text.fontSizeZh + "px 'LiHei Pro','微软正黑体','Microsoft JhengHei'" : t.canvas.font = "bold " + t.dimensions.text.fontSizeEn + "px 'Conv_ITC Avant Garde Gothic LT Bold', 'Ariel Black', 'Ariel'";
-        t.canvas.fillText(word, t.tx, t.ty);
-        t.canvas.rotate(-.8 * Math.PI / 180);
-        t.canvas.closePath();
+            encodeURIComponent(word).length > 1 ? t.canvas.font = "900 " + t.dimensions.text.fontSizeZh + "px 'LiHei Pro','微软正黑体','Microsoft JhengHei'" : t.canvas.font = "bold " + t.dimensions.text.fontSizeEn + "px 'Conv_ITC Avant Garde Gothic LT Bold', 'Ariel Black', 'Ariel'";
+            t.canvas.fillText(word, t.tx, t.ty);
+            t.canvas.closePath();
+        }
+
      });
 
      t.canvas.draw();
